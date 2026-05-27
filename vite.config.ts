5c79b82dd636047bfdfe1b3cd66e027ec5c4fdd9
Cristian <111924880+MagicPhamous@users.noreply.github.com>
2026-05-26 21:20:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { defineConfig } from 'vite'
 
+export default defineConfig({
+  base: '/Sistema-de-Gesti-n-y-Ventas-de-Productos/'
+})
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
